fd6e9a8b575def76ed300e06124195402ede9cd5
Manasvin5 <manasvinshrimali@gmail.com>
2024-06-30 00:32:40 +0530

    idk

--- number-game.c
+++ number-game.c
@@ -5,7 +5,7 @@ int main() {
     int number, guess, attempts = 0;
     srand(time(0));
     number = rand() % 100 + 1;
-    printf("Welcome to the Number Guessing Game!\n");
+    printf("Welcome to the Number Guessing Game!!\n");
     printf("I have selected a number between 1 and 100. Can you guess what it is?\n");
     do {
         printf("Enter your guess: ");
@@ -16,7 +16,7 @@ int main() {
         } else if (guess < number) {
             printf("Too low! Try again.\n");
         } else {
-            printf("Congratulations! You guessed the number in %d attempts.\n", attempts);
+            printf("Congratulations! You guessed the number in %d attempts. Why even are you playing this game\n", attempts);
         }
     } while (guess != number);
     return 0;
