ebd7b64bc9c38809cb21b4890e650713f60e2c50
Manasvin5 <manasvinshrimali@gmail.com>
2024-08-02 23:23:57 +0530

    Hello From Uni

--- divisible-test.c
+++ divisible-test.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 
-
-int main(){
-    //program to check wheather if a number is divisible by 97 or not 
-    printf("program to check wheather if a number is divisible by 97 or not. Enter the number \n");
+int main() {
+    // Program to check whether a number is divisible by 97 or not
+    printf("Program to check whether a number is divisible by 97 or not. Enter the number:\n");
     int a;
-    scanf("%d",&a);
-    if (a%97!=0)) {
-        printf("Number not divisible by")
+    scanf("%d", &a);
+    
+    if (a % 97 != 0) {
+        printf("Number not divisible by 97\n");
+    } else {
+        printf("Number is divisible by 97\n");
     }
 
-
-
-
-
-
-    return 0
+    return 0;
 }
