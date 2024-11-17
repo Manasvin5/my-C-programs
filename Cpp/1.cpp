e84758bd18c96155017f91ce59d4e9b6ed9d4de5
Manasvin5 <manasvinshrimali@gmail.com>
2024-11-17 20:43:36 +0530

    ji

--- Cpp/1.cpp
+++ Cpp/1.cpp
@@ -1,10 +1,14 @@
+#include <codecvt>
 #include <iostream>
+#include <ostream>
 
 int main() {
     std::cout << "Hello,\n World!" << std::endl;
+    
+    int age=800;
+    std::cout<<"This is your age="<<age<< std::endl;
+
     return 0;
 }
-
-
 
 
